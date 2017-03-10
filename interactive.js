716cf0b6ff8bd4e445e33f3e135d63b950b48279
shamaul-d <sdilmohamed@stuy.edu>
2017-03-10 14:43:01 -0500

    looooL

--- interactive.js
+++ interactive.js
@@ -2,34 +2,37 @@ var container = document.getElementById("vimage");
 var clearBtn = document.getElementById("og");
 
 var change = function(e) {
-	console.log("haha")
-}
+	console.log("haha");
+	this.setAttribute('fill', 'green');
+	e.stopPropagation();
+};
 
 var createCircle = function (x,y) {
-	var c =	document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     c.setAttribute("fill", "blue");
     c.setAttribute("cx", x);
     c.setAttribute("cy", y);
     c.setAttribute("r", "20");
     c.addEventListener("click", change);
     return c;
-}
+};
+
 var drawCicle = function(e) {
 	var f = createCircle(event.offsetX, event.offsetY);
 	container.appendChild(f);
-}
+};
 
 
 var clear = function() {
 	var list = document.getElementsByTagName("circle");
 	while (list.length != 0) {
 		container.removeChild(list[0]);
-	}
-}
+	};
+};
 
 var clearOne = function(e) {
 	container.removeChild(e)
-}
+};
 
 
 
