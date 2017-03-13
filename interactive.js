5b25d18fb63c92d7dba23b3beae1432721a12b73
shamaul-d <sdilmohamed@stuy.edu>
2017-03-13 01:56:49 -0400

    almost there

--- interactive.js
+++ interactive.js
@@ -1,5 +1,8 @@
 var container = document.getElementById("vimage");
 var clearBtn = document.getElementById("og");
+var anime = document.getElementById('move');
+var stop = document.getElementById('stop');
+var rid;
 
 var change = function(e) {
 	console.log("haha");
@@ -15,6 +18,8 @@ var createCircle = function (x,y) {
     c.setAttribute("cx", x);
     c.setAttribute("cy", y);
     c.setAttribute("r", "20");
+    c.setAttribute('vx', direction());
+    c.setAttribute('vy', direction());
     c.addEventListener("click", change);
     return c;
 };
@@ -23,6 +28,11 @@ var drawCicle = function(e) {
 	var f = createCircle(event.offsetX, event.offsetY);
 	container.appendChild(f);
 };
+
+var draw = function(x,y) {
+    var l = createCircle(x,y);
+    container.appendChild(l);
+}
 
 var drawrand = function(e) {
     console.log(this);
@@ -45,8 +55,56 @@ var clearOne = function(e) {
 	container.removeChild(e.target);
 };
 
+var move = function(e) {
+	window.cancelAnimationFrame(rid);
 
+        var balls = document.getElementsByTagName("circle");
 
+        console.log(balls);
+
+	var bounce = function() {
+		console.log(rid);
+
+                for (var i = 0; i < balls.length; i++) {
+
+                    var vx = parseInt(balls[i].getAttribute('vx'));
+                    var vy = parseInt(balls[i].getAttribute('vy'));            
+
+                    var x = parseInt(balls[i].getAttribute('cx')) + vx;
+                    var y = parseInt(balls[i].getAttribute('cy')) + vy;
+
+                    if (x >= container.clientWidth || x <= 0) {
+                        vx *= -1;
+                    }
+
+                    if (y >= container.clientHeight || y <= 0) {
+                        vy *= -1;
+                    }
+                    
+                    balls[i].setAttribute('cx',x);
+                    balls[i].setAttribute('cy',y);
+                    balls[i].setAttribute('vx',vx);
+                    balls[i].setAttribute('vy',vy);
+                };
+                rid = window.requestAnimationFrame(bounce);
+	};
+        bounce();
+};
+
+var stopIt = function() {
+    console.log(rid);
+    window.cancelAnimationFrame(rid);
+};
+
+var direction = function() {
+    var h = Math.random();
+    if (h > 0.5) {
+        return 1;
+    }
+    return -1;
+}
 
 container.addEventListener("click", drawCicle);
+anime.addEventListener('click', move);
 clearBtn.addEventListener("click", clear);
+stop.addEventListener('click', stopIt);
